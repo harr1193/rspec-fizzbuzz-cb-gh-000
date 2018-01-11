af4690d5588b4f57cfa10a910fc02ba37e154107
Kyle Harris <k_harris95@live.com>
2018-01-11 01:25:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,10 @@ def fizzbuzz(value)
   if value % 3  == 0
     return "Fizz"
   end
+  if value % 5  == 0
+    return "Buzz"
+  end
+  if (value % 3  == 0) && (value & 5 == 0)
+    return "FizzBuzz"
+  end
 end
